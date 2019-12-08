8583c09422018884cd2deb4781d3321f71f1f31b
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-12-08 09:07:57 +0900

    [master] add first example

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,64 @@
 import React from 'react';
-import logo from './images/logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.scss';
-import dialbird from './images/dialbird.jpg';
-import Button from './components/Button';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={dialbird} alt="" width='200' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
+
+class Home extends React.PureComponent {
+  render() {
+    return <h2>Home</h2>;
+  }
+}
+
+class About extends React.PureComponent {
+  render() {
+    return <h2>About</h2>;
+  }
+}
+
+class Users extends React.PureComponent {
+  render() {
+    return <h2>Users</h2>;
+  }
+}
 
 export default App;
